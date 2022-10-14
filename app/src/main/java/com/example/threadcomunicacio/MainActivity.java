b75155331e2740cebb5cd3245f5d37dc33203a66
SergioHerreraMarin <sergioherreramarin@gmail.com>
2022-10-14 19:25:56 +0200

    Actualizado

--- app/src/main/java/com/example/threadcomunicacio/MainActivity.java
+++ app/src/main/java/com/example/threadcomunicacio/MainActivity.java
@@ -3,6 +3,23 @@ package com.example.threadcomunicacio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,76 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //getDataFromUrl("https://api.myip.com");
+
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Background work here
+                        String res;
+                        res = getDataFromUrl("https://bytes.cat");
+                        Log.i("MYAPP: ",res);
+                        /*handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+
+                            }
+                        });*/
+                    }
+                });
+
+
+
+            }
+        });
+
     }
+
+    private String getDataFromUrl(String demoIdUrl) {
+        String error = ""; // string field
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+
 }
